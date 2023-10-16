c7294ca7e8ab1cb09bfc42c358186598712eb738
Matheus Gusmão <mgusmao412@gmail.com>
2023-10-15 22:11:24 -0300

    feat: created Task model entity

---
+++ src/main/java/me/gusmaomatheus/todolist/todolist/model/Task.java
@@ -0,0 +1,28 @@
+package me.gusmaomatheus.todolist.todolist.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import me.gusmaomatheus.todolist.todolist.enums.TaskPriorityLevel;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50, unique = true)
+    @Size(min = 6, max = 50)
+    private String title;
+    private String description;
+    private Boolean active;
+    @Enumerated(EnumType.STRING)
+    private TaskPriorityLevel priority;
+}
