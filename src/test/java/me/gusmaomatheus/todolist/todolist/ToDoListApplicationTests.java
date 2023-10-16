d35affad3fb2dcb134ab8a0fad5232c8b2a52746
Matheus Gusmão <mgusmao412@gmail.com>
2023-10-15 22:10:37 -0300

    feat: created project and configured dependencies.

---
+++ src/test/java/me/gusmaomatheus/todolist/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gusmaomatheus.todolist.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
