6e5b5e8d7e158b78a739ca7b1014fb85ef2cdfe1
Matheus Gusmão <mgusmao412@gmail.com>
2023-10-15 22:37:48 -0300

    feat: changed request url

--- src/main/java/me/gusmaomatheus/todolist/controller/TaskController.java
+++ src/main/java/me/gusmaomatheus/todolist/controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/task-api")
+@RequestMapping("/task-api/tasks")
 public class TaskController {
 
     @Autowired
