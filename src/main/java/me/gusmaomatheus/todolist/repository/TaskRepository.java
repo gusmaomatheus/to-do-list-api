8722fc5736f5ac5f9bb3e9bd2f2632e187fe449d
Matheus Gusmão <mgusmao412@gmail.com>
2023-10-15 23:11:01 -0300

    fix: also changed the type of id in TaskRepository

--- src/main/java/me/gusmaomatheus/todolist/repository/TaskRepository.java
+++ src/main/java/me/gusmaomatheus/todolist/repository/TaskRepository.java
@@ -3,5 +3,7 @@ package me.gusmaomatheus.todolist.repository;
 import me.gusmaomatheus.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
